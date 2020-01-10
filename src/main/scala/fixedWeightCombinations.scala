6a04f485694b8ffbce811c1df5c3b6ece6feeca8
MaddieBurbage <madeline.g.burbage@gmail.com>
2020-01-10 12:03:18 -0500

    Added response mechanism

--- src/main/scala/fixedWeightCombinations.scala
+++ src/main/scala/fixedWeightCombinations.scala
@@ -1,4 +1,5 @@
 //Simple Combinational Accelerator, based on the fortyTwo accelerator template.
+//Works for string lengths up to xLen - 1 (31? 63?)
 // Maddie Burbage, 2020
 package fixedWeightCombos
 
@@ -14,11 +15,13 @@ class FixedWeightCombos(opcodes: OpcodeSet)(implicit p: Parameters) extends Lazy
 
 class FixedWeightCombosImp(outer: FixedWeightCombos)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
 
+    val unset = ((1 << xLen) - 1).U(xLen.W)
+
     //Get length of binary strings and previous string from the source registers
-    val length = Reg(io.cmd.rs1) //Length of binary string
-    val previous = Reg(io.cmd.rs2) //Previous
-    //Output the new string to the same register containing the previous string
-    val rd = Reg(io.cmd.inst.rs2) //Output location
+    val length = Reg(unset) //Length of binary string
+    val previous = Reg(unset) //Previous
+    //Output the new string to the destination register
+    val rd = Reg(io.cmd.bits.inst.rd) //Output location
 
     //Busy when not ready for a new instruction
     io.busy := ~io.cmd.ready
@@ -26,19 +29,48 @@ class FixedWeightCombosImp(outer: FixedWeightCombos)(implicit p: Parameters) ext
     //When a new command is received, capture inputs and become busy
     when(io.cmd.fire()) {
         io.cmd.ready := 0.U
-        length := io.cmd.rs1
-        previous := io.cmd.rs2
-        rd := io.cmd.inst.rs2
+        length := io.cmd.bits.rs1
+        previous := io.cmd.bits.rs2
+        rd := io.cmd.bits.inst.rd
+        io.resp.valid := Bool(false)
     }
 
+    //After responding, become ready for new commands
     when(io.resp.fire()) {
         io.cmd.ready := 1.U
+        length := Reg(unset)
+        previous := Reg(unset)
     }
 
-    //response
-    io.resp.valid :=
+    //Calculations
+    val trimmed = previous & (previous + 1)
+    val trailed = trimmed ^ (trimmed - 1)
+
+    val indexShift = trailed + 1
+    val indexTrailed = trailed & previous
+
+    val subtracted = (indexShift & previous) - 1
+    val fixed = Mux(SInt(subtracted) > 0, subtracted, 0.U) //make signed?
+
+    val result = previous + indexTrailed - fixed
+
+    //Response not ready until result value has changed from the default
+    when(result =/= unset - 1) {
+        io.resp.valid := Bool(true)
+    }
+
+    val stopper = 1 << length
+
+    //Fill result with all 1s if finished, same response as divide by zero has
+    when(result >> length === 0.U) {
+        io.resp.bits.data := ~ (0.U)
+    } .otherwise {
+        io.resp.bits.data := result(length - 1, 0)
+    }
+
+
+    //Response
     io.resp.bits.rd := rd
-    io.resp.bits.data :=
     io.interrupt := Bool(false)
     io.mem.req.valid := Bool(false)
 
