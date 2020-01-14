a86c482c9e20e5fe2345d192e1da578f9d41c18d
MaddieBurbage <madeline.g.burbage@gmail.com>
2020-01-14 01:34:49 -0500

    Started incorporating function codes

--- src/main/scala/fixedWeightCombinations.scala
+++ src/main/scala/fixedWeightCombinations.scala
@@ -8,16 +8,17 @@ import freechips.rocketchip.config._ //For Config
 import freechips.rocketchip.diplomacy._ //For LazyModule
 import freechips.rocketchip.rocket.{TLBConfig, HellaCacheReq} //something and level one cache
 
-class FixedWeightCombinations(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes) {
-    override lazy val module = new FixedWeightCombinationsImp(this)
+class Combinations(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes) {
+    override lazy val module = new CombinationsImp(this)
 }
 
-//Generates a fixed-weight binary string based on a previous string of the same
-//weight and length. Binary strings up to length 32 will work.
-class FixedWeightCombinationsImp(outer: FixedWeightCombinations)(implicit p: Parameters) extends LazyRoCCModuleImp(outer){
+class CombinationsImp(outer: Combinations)(implicit p: Parameters) extends LazyRoCCModuleImp(outer){
 
     val s_idle :: s_resp :: Nil = Enum(Bits(), 2)
     val state = Reg(init = s_idle) //state starts idle, is remembered
+
+    val fixedWeightModule = LazyModule(new FixedWeight())
+
 
     //Instruction inputs
     val length = Reg(io.cmd.bits.rs1(4,0)) //Length of binary string
@@ -25,6 +26,11 @@ class FixedWeightCombinationsImp(outer: FixedWeightCombinations)(implicit p: Par
 
     //Secondary Inputs
     val rd = Reg(io.cmd.bits.inst.rd) //Output location
+    val function = Reg(io.cmd.bits.inst.funct) //Specific operation
+
+    //Set up submodule inputs
+    fixedWeightModule.io.length := length
+    fixedWeightModule.io.previous := previous
 
     //State-based communication values
     io.cmd.ready := (state === s_idle)
@@ -36,26 +42,13 @@ class FixedWeightCombinationsImp(outer: FixedWeightCombinations)(implicit p: Par
         length := (io.cmd.bits.rs1(4,0))
         previous := io.cmd.bits.rs2
         rd := io.cmd.bits.inst.rd
+        function := io.cmd.bits.inst.funct
     }
 
+    //Obtain accelerator output from the correct submodule for the function
+    io.resp.bits.data := MuxLookup(function, FixedWeight.io.out,
+            Array(0.U->FixedWeight.io.out, 1.U->Lexicographic.io.out))
 
-    //Calculations to generate the next combination
-    val trimmed = previous & (previous + 1.U)
-    val trailed = trimmed ^ (trimmed - 1.U)
-
-    val indexShift = trailed + 1.U
-    val indexTrailed = trailed & previous
-
-    val subtracted = (indexShift & previous) - 1.U
-    val fixed = Mux(subtracted.asSInt < 0.S, 0.U, subtracted)
-
-    val result = previous + indexTrailed - fixed
-
-    val stopper = 1.U(1.W) << length
-
-
-    //Fill result with all 1s if finished
-    io.resp.bits.data := Mux(result >> length =/= 0.U, ~(0.U), result % stopper)
     io.resp.bits.rd := rd
     io.resp.valid := (state === s_resp)
 
@@ -70,21 +63,58 @@ class FixedWeightCombinationsImp(outer: FixedWeightCombinations)(implicit p: Par
 
 }
 
-class WithFixedWeightCombinations extends Config((site, here, up) => {
+//Generates a fixed-weight binary string based on a previous string of the same
+//weight and length. Binary strings up to length 32 will work.
+class FixedWeight()(implicit p: Parameters) extends Submodule {
+
+    //Calculations to generate the next combination
+    val trimmed = io.previous & (io.previous + 1.U)
+    val trailed = trimmed ^ (trimmed - 1.U)
+
+    val indexShift = trailed + 1.U
+    val indexTrailed = trailed & io.previous
+
+    val subtracted = (indexShift & io.previous) - 1.U
+    val fixed = Mux(subtracted.asSInt < 0.S, 0.U, subtracted)
+
+    val result = io.previous + indexTrailed - fixed
+
+    val stopper = 1.U(1.W) << io.length
+
+    //Fill result with all 1s if finished
+    io.out := Mux(result >> io.length =/= 0.U, ~(0.U), result % stopper)
+}
+
+//Generates the lexicographically-next binary string
+class Lexicographic()(implicit p: Parameters) extends Submodule {
+    val result = io.previous + 1.U
+    io.out := Mux((result >> length) & 1.U === 1, ~(0.U), result)
+}
+
+//Base class for this accelerator's submodules
+abstract class Submodule()(implicit p: Parameters) extends LazyModule {
+    val io = IO(new Bundle {
+        val length = Input(UInt(5.W))
+        val previous = Input(UInt(32.W))
+        val out = Output(UInt(32.W))
+    })
+}
+
+class WithCombinations extends Config((site, here, up) => {
     case BuildRoCC => Seq((p: Parameters) => {
-        val fixedWeightCombinations = LazyModule.apply(new FixedWeightCombinations(OpcodeSet.custom0) (p))
-        fixedWeightCombinations
+        val Combinations = LazyModule.apply(new Combinations(OpcodeSet.custom0) (p))
+        Combinations
     })
 })
 
 /**
  * Add this into the example project's RocketConfigs.scala file:
 
-class FixedWeightCombinationsRocketConfig extends Config(
+class CombinationsRocketConfig extends Config(
     new WithTop ++
     new WithBootROM ++
     new freechips.rocketchip.subsystem.WithInclusiveCache ++
-    new fixedWeightCombinations.WithFixedWeightCombinations ++
+    new fixedWeightCombinations.WithCombinations ++
     new freechips.rocketchip.subsystem.WithNBigCores(1) ++
     new freechips.rocketchip.system.BaseConfig
 )
