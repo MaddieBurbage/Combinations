4476fa3623070d3abd0747e716a8797f7547a808
MaddieBurbage <madeline.g.burbage@gmail.com>
2020-01-16 23:07:09 -0500

    Finished general combinations accelerator and c code

--- tests/generalCombinations.c
+++ tests/generalCombinations.c
@@ -1,6 +1,7 @@
 // Tests for the general binary combinations accelerator
 // (c) Maddie Burbage, 2020
 
+
 #include "rocc.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,20 +12,24 @@
  * Way to Generate Binary Strings"
  */
 int nextGeneralCombination(int n, int last, int *out) {
-    unsigned int next, mask, lastTemporary, lastLimit, lastPosition, first, shifted, rotated, result;
+  unsigned int cut, trimmed, trailed, mask, lastTemp, lastLimit, lastPosition, cap, first, shifted, rotated, result;
 
-    next = last ^ (last + 1);
-    mask = (next > 3)? next : ~(0);
+    cut = last >> 1;
+    trimmed = cut | (cut - 1); //Discards trailing zeros
+    trailed = trimmed ^ (trimmed + 1); //Marks the start of the last "01"
+    mask = (trailed << 1) + 1;
+    
+    lastTemp = trailed + 1; //Indexes the start of the last "01"
+    lastLimit = 1 << (n-1); //Indexes the length of the string
+    lastPosition = (lastTemp == 0 || lastTemp > lastLimit)? lastLimit : lastTemp;
 
-    lastTemporary = (mask >> 1) + 1;
-    lastLimit = 1 << (n-1);
-    lastPosition = (lastTemporary > lastLimit)? lastLimit : lastTemporary;
-    first = (next > 3)? 1 & last : 1 & ~(last);
-    shifted = (last & mask) >> 1;
+    cap = 1 << n;
+    first = (mask < cap)? 1 & last : 1 & ~(last); //The bit to be moved
+    shifted = cut & trailed;
     rotated = (first == 1)? shifted | lastPosition : shifted;
     result = rotated | (~mask & last);
 
-    if(result == (1 << n) - 1) {
+    if(result == cap - 1) {
         return -1;
     }
 
@@ -53,8 +58,8 @@ static inline int testAccelerator(int inputString, int length) {
 }
 
 int main(void) {
-    int inputString = 0b0;
-    int length = 4;
+    int inputString = 0b0111;
+    int length = 16;
 
 
     int testResult = testAccelerator(inputString, length);
