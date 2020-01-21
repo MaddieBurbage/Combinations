cdf3e2836bd295c5f07c37eb15a1003b1afcf164
MaddieBurbage <madeline.g.burbage@gmail.com>
2020-01-21 10:25:34 -0500

    Rough draft of memory accessing

--- src/main/scala/combinations.scala
+++ src/main/scala/combinations.scala
@@ -6,7 +6,7 @@ import Chisel._
 import freechips.rocketchip.tile._ //For LazyRoCC
 import freechips.rocketchip.config._ //For Config
 import freechips.rocketchip.diplomacy._ //For LazyModule
-import freechips.rocketchip.rocket.{TLBConfig, HellaCacheReq} //something and level one cache
+import freechips.rocketchip.rocket.{TLBConfig, HellaCacheReq} //For connections
 
 class Combinations(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes) {
     override lazy val module = new CombinationsImp(this)
@@ -14,13 +14,15 @@ class Combinations(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(
 
 class CombinationsImp(outer: Combinations)(implicit p: Parameters) extends LazyRoCCModuleImp(outer){
 
-    val s_idle :: s_resp :: Nil = Enum(Bits(), 2)
+    val s_idle :: s_work :: s_resp :: Nil = Enum(Bits(), 3)
     val state = Reg(init = s_idle) //state starts idle, is remembered
 
 
     val submodules = Array(Module(new FixedWeight()(p)), Module(new Lexicographic()(p)), Module(new GeneralCombinations()(p)))
     val rangedCombinations = Module(new RangedCombinations()(p))
     val captureWeights = Module(new CaptureWeights()(p))
+    val memoryCombinations = Module(new MemoryFixedCombinations()(p))
+
     //Instruction inputs
     val length = Reg(io.cmd.bits.rs1(4,0)) //Length of binary string
     val previous = Reg(io.cmd.bits.rs2) //Previous binary string
@@ -38,20 +40,33 @@ class CombinationsImp(outer: Combinations)(implicit p: Parameters) extends LazyR
     captureWeights.io.newMax := previous
     rangedCombinations.io.length := length
     rangedCombinations.io.previous := previous
+    memoryCombinations.io.length := length
+    memoryCombinations.io.address := previous
+
+    //Unique submodule inputs
     rangedCombinations.io.minWeight := captureWeights.io.minWeight
     rangedCombinations.io.maxWeight := captureWeights.io.maxWeight
     captureWeights.io.reset := Mux(function === 3.U && io.resp.bits.data === ~(0.U(64.W)), 1.U, 0.U)
     captureWeights.io.set := Mux(function === 4.U, 1.U, 0.U)
 
 
-
     //State-based communication values
     io.cmd.ready := (state === s_idle)
-    io.busy := (state =/= s_idle)
 
     //When a new command is received, capture inputs and become busy
     when(io.cmd.fire()) {
-        state := s_resp
+	when(function === 3.U) {
+	    state := s_work
+	    memoryCombinations.io.start := 1.U
+	} .otherwise {
+            state := s_resp
+	}
+    } .otherwise {
+	memoryCombinations.io.start := 0.U
+    }
+
+    when(state === s_work && memoryCombinations.io.working === 0.U) {
+	state := s_resp
     }
 
     length := (io.cmd.bits.rs1(4,0))
@@ -61,24 +76,46 @@ class CombinationsImp(outer: Combinations)(implicit p: Parameters) extends LazyR
 
     //Obtain accelerator output from the correct submodule for the function
     val lookups = Array(0.U->submodules(0).io.out,1.U->submodules(1).io.out, 2.U->submodules(2).io.out,
-        3.U->rangedCombinations.io.out, 4.U->captureWeights.io.success)
+        3.U->memoryCombinations.io.out, 4.U->rangedCombinations.io.out, 5.U->captureWeights.io.success)
 //  val newLookups = lookups.zipWithIndex.map(_.swap)}
     io.resp.bits.data := MuxLookup(function, submodules(0).io.out, lookups)
 
-
-
     io.resp.bits.rd := rd
-    io.resp.valid := (state === s_resp)
+    io.resp.valid := state === s_resp
 
     //After responding, become ready for new commands
     when(io.resp.fire()) {
         state := s_idle
     }
 
-    //These features not used
+    //Always false
     io.interrupt := Bool(false)
-    io.mem.req.valid := Bool(false)
 
+
+    //Memory request interface
+    io.mem.req.valid := memoryCombinations.io.working
+    io.busy := memoryCombinations.io.working
+    io.mem.req.bits.addr := memoryCombinations.io.currentAddress
+    io.mem.req.bits.tag := memoryCombinations.io.out(9,0) //Change for out-of-order
+    io.mem.req.bits.cmd := 0.U //change when actually storing
+    io.mem.req.bits.data := Bits(0) //Also changed
+    io.mem.req.bits.size := log2Ceil(32).U
+    io.mem.req.bits.signed := Bool(false)
+    io.mem.req.bits.phys := Bool(false)
+
+    when(io.mem.resp.valid) {
+	memoryCombinations.io.mem.respValid := 1.U
+	memoryCombinations.io.mem.respData := io.mem.resp.bits.data
+	memoryCombinations.io.mem.respTag := io.mem.resp.bits.tag
+    } .otherwise {
+	memoryCombinations.io.mem.respValid := 0.U
+    }
+
+    when(io.mem.req.fire()) {
+	memoryCombinations.io.mem.reqFire := 1.U
+    } .otherwise {
+	memoryCombinations.io.mem.reqFire := 0.U
+    }
 }
 
 //Generates a fixed-weight binary string based on a previous string of the same
@@ -102,6 +139,75 @@ class FixedWeight()(implicit p: Parameters) extends Module {
 
     //Fill result with all 1s if finished
     io.out := Mux(result >> io.length =/= 0.U, Fill(64,1.U), result % stopper)
+}
+
+//Generates all binary strings based on an input and "saves" it to memory.
+//Strings of length up to 32 work.
+class MemoryFixedCombinations()(implicit p: Parameters) extends Module() {
+    val io = IO(new Bundle { //address and length
+	val length = Input(UInt(5.W))
+	val address = Input(UInt(64.W))
+	val mem = Input(new Bundle {
+	    val respValid = Input(UInt(1.W))
+	    val respTag = Input(UInt(10.W))
+	    val respData = Input(UInt(32.W))
+	    val reqFire = Input(UInt(1.W))
+	})
+	val start = Input(UInt(1.W))
+	val currentAddress = Output(UInt(64.W))
+	val out = Output(UInt(64.W))
+	val working = Output(UInt(1.W))
+    })
+
+    val s_store ::  s_idle :: Nil = Enum(Bits(), 2)
+    val state = Reg(init = s_idle)
+
+    val initial = (1.U << io.length) - 1.U
+    val nextSent = Reg(UInt(64.W))
+
+    when(io.start === 1.U) {
+	state := s_store
+	nextSent := initial
+    }
+
+    when(nextSent === ~(0.U(64.W))) {
+	state := s_idle
+    }
+    
+    //Calculations
+    val trimmed = nextSent(31,1) | (nextSent(31,1) - 1.U)
+    val trailed = trimmed ^ (trimmed + 1.U)
+    val mask = Wire(UInt(32.W))
+    mask := (trailed << 1.U) + 1.U
+    val lastTemp = Wire(UInt(32.W))
+    lastTemp :=  trailed + 1.U
+    val lastLimit = 1.U << (io.length - 1.U)
+    val lastPosition = Mux(lastTemp > lastLimit || lastTemp === 0.U, lastLimit, lastTemp)
+    val cap = 1.U << io.length
+    val first = Mux(mask < cap, 1.U & nextSent, 1.U & ~nextSent)
+    val shifted = (nextSent & mask) >> 1.U
+    val rotated = Mux(first === 1.U, shifted | lastPosition, shifted)
+    val result = rotated | (~mask & nextSent)
+    val stopper = 1.U(1.W) << io.length
+
+
+    val offset = Reg(init = 0.U)
+    val sum = Reg(init = 0.U)
+    
+    //Outputs
+    nextSent := Mux(result >> io.length =/= 0.U, Fill(64,1.U), result % stopper)
+    io.out := sum
+    io.currentAddress := io.address + offset
+
+    //Signals to accelerator
+    io.working := state === s_store
+
+    //After getting response from memory
+    when(io.mem.respValid === 1.U) {
+	sum := sum + io.mem.respData
+	nextSent := result
+	offset := offset + 32.U
+    }
 }
 
 //Generates the lexicographically-next binary string, up to a length of 32
