62354fce1cd1717552d2e971bcc5d950a8a75ce8
MaddieBurbage <madeline.g.burbage@gmail.com>
2021-05-24 09:57:51 -0400

    Fixed script

--- tests/generate.sh
+++ tests/generate.sh
@@ -5,7 +5,7 @@ WIDTHS=(2 4 8 14 16 20 24 32)
 export FUNCT=0
 while [ $FUNCT -lt 3 ]; do
     WIDTHI=0
-    export WIDTH = ${WIDTHS[0]}
+    export WIDTH=${WIDTHS[0]}
     while [ $WIDTHI -lt $MAX ]; do
 	WIDTH=${WIDTHS[$WIDTHI]}
 	export WARE=0
@@ -24,7 +24,7 @@ MAX=4
 FUNCT=4
 while [ $FUNCT -lt 7 ]; do
     WIDTHI=0
-    WIDTH = ${WIDTHS[0]}
+    WIDTH=${WIDTHS[0]}
     while [ $WIDTHI -lt $MAX ]; do
         WIDTH=${WIDTHS[$WIDTHI]}
 	WARE=0
